9199589e97502b1e971f09486153f06497690f30
Website-Bros123 <websitebros2026@gmail.com>
2026-08-06 17:52:53 -0700

    Create sitemap function for site URLs
    This file defines a sitemap function that returns an array of URLs with their last modified dates.

---
+++ app/sitemaps.js
@@ -0,0 +1,44 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://abccare2daycare.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/gallery",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/safety",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/curriculum",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/subsidized-child-care",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/partnership",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/faq",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abccare2daycare.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
